e5733d948b25bba1e51ded2a01d66ff4376791e6
Irosh Perera <iroshlashan31@gmail.com>
2024-05-23 01:56:49 +0530

    few changes

---
+++ pages/js/index-page.js
@@ -0,0 +1,50 @@
+$('#btn-user-login').click(function (event) {
+    event.preventDefault();  // Pass the event object to the function and prevent the default form submission
+
+    let username = $('#txt-user-email').val();
+    let password = $('#txt-user-password').val();
+
+    const user = {
+        email: username,
+        password: password
+    };
+
+    $.ajax({
+        method: 'POST',
+        url: 'http://localhost:8080/api/v1/user/signin',
+        contentType: 'application/json',
+        data: JSON.stringify(user),
+        success: function (token) {
+            if (!token || token === '') {
+                alert('Invalid username or password');
+                return;
+            }
+            const tokens = token.token.split(" : ");
+            const token1 = tokens[0].trim();
+            const token2 = tokens[1].trim();
+
+
+            localStorage.removeItem('token');
+            localStorage.removeItem('refresh-token');
+
+            localStorage.setItem('token', token1);
+            localStorage.setItem('refresh-token', token2);
+            $.ajax({
+                method: 'GET',
+                url: 'http://localhost:8080/api/v1/user/role?email=' + username,
+                success: function (role) {
+
+
+                    localStorage.removeItem('role');
+                    localStorage.setItem('role', role);
+                    window.location.href = 'pages/main_page.html';
+                }
+            });
+
+
+        },
+        error: function (error) {
+            alert('Invalid username or password');
+        }
+    });
+});
